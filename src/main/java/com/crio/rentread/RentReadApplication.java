931d188d20b1d3adaa33f88bdfab9f4fb708b6c3
vishal-code-create <vc110497@gmail.com>
2024-08-28 15:06:21 +0530

    authentication setup

---
+++ src/main/java/com/crio/rentread/RentReadApplication.java
@@ -0,0 +1,13 @@
+package com.crio.rentread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentReadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentReadApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/crio/rentread/dto/UserRegistrationDetails.java
@@ -0,0 +1,13 @@
+package com.crio.rentread.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRegistrationDetails {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+
+}
